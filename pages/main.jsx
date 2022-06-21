af3a70d4f3ec4518e8472f5858a7e61dc444dd7e
Atlas238 <jack@bentsen.us>
2022-06-20 17:41:51 -0700

    small tweaks, finalizing outline

--- pages/main.jsx
+++ pages/main.jsx
@@ -1,11 +1,61 @@
+import { useState } from 'react'
+import NewClientForm from '../components/NewClientForm'
+import LookupForm from '../components/LookupForm'
 import styles from '../styles/Home.module.css'
 export default function Main() {
 
+    // View Prop - Controls render
+    const [view, setView] = useState('main')
+
+    // New Client Prop
+    const [newClient, setNewClient] = useState({
+        firstName: "",
+        lastName: "",
+        middleInitial: "",
+        dobMonth: 1,
+        dobDay: 1,
+        dobYear: 1900,
+        zipcode: 123456,
+        race: '',
+        gender: '',
+        numInFamily: 0
+    })
+
     const logout = (e) => 
     {
-       // Wipe localstorage volunteer code
+       // Wipe localstorage volunteer code, return to login page
        localStorage.clear()
        window.location = '/'
+    }
+
+    // Renders Options based on view prop
+    const renderView = (view) => 
+    {
+        switch (view) 
+        {
+            case 'main':
+                return (
+                    <main>
+                        <button onClick={()=>{ setView('add') }}>Add a new Client</button>
+                        <button onClick={()=>{ setView('lookup') }}>Look up an existing Client</button>
+                    </main>
+                )
+            case 'add':
+                return (
+                    <>
+                        <NewClientForm newClient={newClient} setNewClient={setNewClient}/>
+                        <button onClick={()=>{ setView('main') }}>Back</button>
+                    </>
+                )
+            
+            case 'lookup':
+                return (
+                    <>
+                        <LookupForm />
+                        <button onClick={()=>{ setView('main') }}>Back</button>
+                    </>
+                )
+        }
     }
 
     return (
@@ -17,10 +67,7 @@ export default function Main() {
                 <p>What would you like to do?</p>
             </header>
 
-            <main>
-                <button>Add a new Client</button>
-                <button>Look up an existing Client</button>
-            </main>
+            {renderView(view)}
 
             <footer>
                 <button onClick={logout}>Log out</button>
