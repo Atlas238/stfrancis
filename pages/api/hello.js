d33d1509314602bfb4d402afd85c8f7d9c3ba72e
Atlas238 <jack@bentsen.us>
2022-06-24 12:09:13 -0700

    cleanup + prep for tailwind

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const file = require('../../tempdata/Clients.json')
+  console.log(file)
+  res.status(200).json(clientFile)
 }
--- pages/main.jsx
+++ pages/main.jsx
@@ -1,25 +1,9 @@
 import { useState } from 'react'
-import NewClientForm from '../components/NewClientForm'
-import LookupForm from '../components/LookupForm'
-import styles from '../styles/Home.module.css'
-export default function Main() {
+
+export default function Main({ clientsData }) {
 
     // View Prop - Controls render
     const [view, setView] = useState('main')
-
-    // New Client Prop
-    const [newClient, setNewClient] = useState({
-        firstName: "",
-        lastName: "",
-        middleInitial: "",
-        dobMonth: 1,
-        dobDay: 1,
-        dobYear: 1900,
-        zipcode: 123456,
-        race: '',
-        gender: '',
-        numInFamily: 0
-    })
 
     const logout = (e) => 
     {
@@ -28,38 +12,8 @@ export default function Main() {
        window.location = '/'
     }
 
-    // Renders Options based on view prop
-    const renderView = (view) => 
-    {
-        switch (view) 
-        {
-            case 'main':
-                return (
-                    <main>
-                        <button onClick={()=>{ setView('add') }}>Add a new Client</button>
-                        <button onClick={()=>{ setView('lookup') }}>Look up an existing Client</button>
-                    </main>
-                )
-            case 'add':
-                return (
-                    <>
-                        <NewClientForm newClient={newClient} setNewClient={setNewClient}/>
-                        <button onClick={()=>{ setView('main') }}>Back</button>
-                    </>
-                )
-            
-            case 'lookup':
-                return (
-                    <>
-                        <LookupForm />
-                        <button onClick={()=>{ setView('main') }}>Back</button>
-                    </>
-                )
-        }
-    }
-
     return (
-        <div className={styles.container}>
+        <div>
 
             <h2>User: Test</h2>
 
@@ -67,11 +21,19 @@ export default function Main() {
                 <p>What would you like to do?</p>
             </header>
 
-            {renderView(view)}
-
             <footer>
                 <button onClick={logout}>Log out</button>
             </footer>
         </div>
     )
 }
+
+// export async function getStaticProps(context)
+// {
+//     // HUUUGE! Thus why we use a DB (: keep commented out if you dont wanna freeze things
+//     let clientsData = await require('../tempdata/Clients.json')
+
+//     return {
+//         props: { clientsData }
+//     }
+// }
