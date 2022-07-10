abb2f735805599756b30253e21d867935e38b513
Atlas238 <jack@bentsen.us>
2022-07-09 18:10:50 -0700

    removing old stuff

--- pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  const file = require('../../tempdata/Clients.json')
-  console.log(file)
-  res.status(200).json(clientFile)
-}
