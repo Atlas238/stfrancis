cd92c7a5f390b1b8385cfb18c1ce766d9a468f77
Atlas238 <jack@bentsen.us>
2022-07-09 18:11:30 -0700

    final edits

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,71 @@
-import { AppWrapper } from '../contexts/State'
-import '../styles/globals.css'
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-function MyApp({ Component, pageProps }) {
-  return (
-  <AppWrapper>
-    <Component { ...pageProps } />
-  </AppWrapper>
-  )
+import '../styles/globals.css';
+import { userService } from '../services/user.service';
+import { Nav } from '../components/Nav';
+
+export default App;
+
+function App({ Component, pageProps }) {
+    const router = useRouter();
+    const [authorized, setAuthorized] = useState(false);
+
+    useEffect(() => {
+        // run auth check on initial load
+        authCheck(router.asPath);
+
+        // set authorized to false to hide page content while changing routes
+        const hideContent = () => setAuthorized(false);
+        router.events.on('routeChangeStart', hideContent);
+
+        // run auth check on route change
+        router.events.on('routeChangeComplete', authCheck)
+
+        // unsubscribe from events in useEffect return function
+        return () => {
+            router.events.off('routeChangeStart', hideContent);
+            router.events.off('routeChangeComplete', authCheck);
+        }
+    }, []);
+
+    function authCheck(url) {
+        // redirect to login page if accessing a private page and not logged in 
+        const publicPaths = ['/login'];
+        const path = url.split('?')[0];
+        if (!userService.userValue && !publicPaths.includes(path)) {
+            setAuthorized(false);
+            router.push({
+                pathname: '/login',
+                query: { returnUrl: router.asPath }
+            });
+        } else {
+            setAuthorized(true);
+        }
+    }
+
+    return (
+        <>
+            <Head>
+                <title>St Francis House</title>
+            </Head>
+
+            <div className="container">
+                <Nav />
+                <div className="container px-2 py-4">
+                    {authorized &&
+                        <>
+                            <Component {...pageProps} />
+                        </>
+                    }
+                </div>
+            </div>
+
+            {/* credits */}
+            <div className="text-center mt-4">
+                <p>Copyright 2022 : The Saint Francis House Foundation</p>
+            </div>
+        </>
+    );
 }
-
-export default MyApp
