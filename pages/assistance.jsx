2d4fffd48a70458311b26f3e35847ce3f8bdd251
Fan Yang <fanyang32@hotmail.com>
2022-07-17 19:33:45 -0700

    fixed assistance buttons

--- pages/assistance.jsx
+++ pages/assistance.jsx
@@ -18,7 +18,10 @@ function Home() {
             <div>
                 <div class="absolute inset-y-5 right-5">
                 <NavLink href= "/index" className="btn rounded-full bg-red-500 border-red-600 hover:bg-red-700">Back</NavLink>
-                <button className="btn rounded-full bg-blue-500 border-blue-600 hover:bg-blue-700">Reprint Receipts</button>
+                <button className="btn rounded-full bg-blue-500 border-blue-600 hover:bg-blue-700">Enter Items</button>
+                <button className="btn rounded-full bg-blue-500 border-blue-600 hover:bg-blue-700">Item Lookup</button>
+                <button className="btn rounded-full bg-blue-500 border-blue-600 hover:bg-blue-700">Print Receipt</button>
+                <button className="btn rounded-full bg-blue-500 border-blue-600 hover:bg-blue-700">Member Lookup</button>
                 <button className="btn rounded-full bg-blue-500 border-blue-600 hover:bg-blue-700">Sign Out</button>
                 </div>
             </div>
