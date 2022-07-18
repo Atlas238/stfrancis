ecf1b955f64ee1600719f71e930bfa708257f794
Atlas238 <jack@bentsen.us>
2022-07-18 12:22:25 -0700

    fixing className

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,10 +14,10 @@ function Home() {
     }, []);
 
     return (
-        <main class="flex min-w-full min-h-screen border-red-500 border-2">
+        <main className="flex min-w-full min-h-screen border-red-500 border-2">
             <div>
-                <div class="absolute top-50 right-0 flex col-span-2 gap-1 border-purple-700 border-2">
-                    <div class="grid grid-cols-2"> 
+                <div className="absolute top-50 right-0 flex col-span-2 gap-1 border-purple-700 border-2">
+                    <div className="grid grid-cols-2"> 
                         <div class= "flex flex-col justify-end row-span-2 gap-1 border-gray-600 border-2">
                             <NavLink href= "#" className="btn rounded-full bg-red-500 border-red-600 hover:bg-red-700">Back</NavLink>
                             <NavLink href= "/requests" className="btn rounded-full bg-blue-500 border-blue-600 hover:bg-blue-700">Requests</NavLink>
