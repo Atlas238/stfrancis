0baaac179fa8606e8bd6a620353df9a16b2637fb
Jack Bentsen <jack@bentsen.us>
2022-07-21 16:02:17 -0700

    Removed checkin button from nav, made it fixed

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -34,14 +34,14 @@ function Nav() {
     if (!user) return null
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 fixed z-10">
             <div className="flex-1">
                 <NavLink className="btn btn-ghost normal-case text-xl" href={"/"}>Home</NavLink>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
                     {/* Render diff navlinks based on location variable */}
-                    <li><NavLink href={"/checkin"} className="btn btn-primary m-1">Check In</NavLink></li>
+                    {/* <li><NavLink href={"/checkin"} className="btn btn-primary m-1">Check In</NavLink></li> */}
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-secondary m-1">Theme</label>
                         <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
@@ -50,7 +50,7 @@ function Nav() {
                             <li><a onClick={setThemeRainbow}>Rainbow</a></li>
                         </ul>
                     </div>
-                    <li><a onClick={logout} className="btn btn-accent m-1">Logout</a></li>
+                    <li><a onClick={logout} className="btn btn-accent text-primary-content m-1">Logout</a></li>
                 </ul>
             </div>
         </div>
