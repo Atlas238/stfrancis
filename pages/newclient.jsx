69569dfed205028a6225dc07d05d7301d60c202b
J.H <59863142+JoenHo@users.noreply.github.com>
2022-07-20 19:39:43 -0700

    add submit handler and Yup from validation

--- pages/newclient.jsx
+++ pages/newclient.jsx
@@ -1,39 +1,64 @@
 import { NavLink } from '../components/NavLink.jsx';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
+// form validation
+const clientSchema = Yup.object().shape({
+    firstName: Yup.string().required(),
+    lastName: Yup.string().required(),
+    middleInitial: Yup.string().notRequired().when('middleInitial', {is:(value) => value?.length, then:(rule) => rule.length(1)}),
+    dateOfBirth: Yup.date().required(),
+    gender: Yup.string().notRequired(),
+    race: Yup.string().notRequired(),
+    postalCode: Yup.number().notRequired().when('postalCode', {is:(value) => value?.length, then:(rule) => rule.length(5)}),
+    familyId: Yup.string().notRequired()
+},
+// add cyclic dependencies for requiring itself
+[['middleInitial', 'middleInitial'],['postalCode', 'postalCode']]);
 
 export default function newclient() {
+
+    const { register, handleSubmit, errors } = useForm({
+        resolver: yupResolver(clientSchema)
+    });
+
+    const submitForm = (data) => {
+        console.log(data)
+    }
 
     return (
         <div>
             <h1 className="py-8 font-bold text-white-700 text-center text-2xl">New Client Form</h1>
-            <form className="px-40 py-4">
+            <form onSubmit={handleSubmit(submitForm)} className="px-40 py-4">
                 <div className="shadow overflow-hidden sm:rounded-md">
                     <div className="px-9 py-5 bg-white sm:p-6">
                         <div className="p-12 grid grid-cols-9 gap-12">
                         {/* First Name */}
                         <div className="col-span-9 sm:col-span-3">
                             <label className="block font-bold text-gray-700">First name</label>
-                            <input type="text" name="firstName" className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
+                            <input type="text" name="firstName" {...register('firstName')} className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
                         </div>
                         {/* Last Name */}
                         <div className="col-span-9 sm:col-span-3">
                             <label className="block font-bold text-gray-700">Last name</label>
-                            <input type="text" name="lastName" className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
+                            <input type="text" name="lastName" {...register('lastName')} className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
                         </div>
                         {/* Middle Initial */}
                         <div className="col-span-9 sm:col-span-3">
                             <label className="block font-bold text-gray-700">Middle Initial</label>
-                            <input type="text" name="middleInitial" className="flex-1 w-1/3 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
+                            <input type="text" name="middleInitial" {...register('middleInitial')} className="flex-1 w-1/3 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
                         </div>
                         {/* Date of Birth */}
                         <div className="col-span-9 sm:col-span-3">
                             <label className="block font-bold text-gray-700">Date of Birth</label>
-                            <input type="date" name="dateOfBirth" placeholder="date" className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none"></input>
+                            <input type="date" name="dateOfBirth" {...register('dateOfBirth')} placeholder="date" className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none"></input>
                         </div>
                         {/* Gender */}
                         <div className="col-span-9 sm:col-span-3">
                             <label className="block font-bold text-gray-700">Gender</label>
-                            <select name="gender" className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none">
-                                <option defaultValue>(Optional)</option>
+                            <select name="gender" {...register('gender')} className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none">
+                                <option defaultValue value="">(Optional)</option>
                                 <option>F</option>
                                 <option>M</option>
                                 <option>X</option>
@@ -42,8 +67,8 @@ export default function newclient() {
                         {/* Race */}
                         <div className="col-span-9 sm:col-span-3">
                             <label className="block font-bold text-gray-700">Race</label>
-                            <select name="race" className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none">
-                                <option defaultValue>(Optional)</option>
+                            <select name="race" {...register('race')} className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none">
+                                <option defaultValue value="">(Optional)</option>
                                 <option>American Indian or Alaska Native</option>
                                 <option>Asian</option>
                                 <option>Black or African American</option>
@@ -56,12 +81,12 @@ export default function newclient() {
                         {/* Zip Code */}
                         <div className="col-span-9 sm:col-span-3">
                             <label className="block font-bold text-gray-700">Postal code</label>
-                            <input type="text" name="postalCode" className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
+                            <input type="text" name="postalCode" {...register('postalCode')} className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
                         </div>
                         {/* Family */}
                         <div className="col-span-9 sm:col-span-3">
                             <label className="block font-bold text-gray-700">Family Id</label>
-                            <input type="text" name="familyId" className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
+                            <input type="text" name="familyId" {...register('familyId')} className="flex-1 w-3/4 py-2 text-center border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
                         </div>
                         </div>
                     </div>
