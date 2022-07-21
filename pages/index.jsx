e584399d5042f20076c981437527687071d7060d
Jack Bentsen <jack@bentsen.us>
2022-07-21 11:26:53 -0700

    commiting pulled changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,23 +15,23 @@ function Home() {
 
     return (
         <div className="flex min-w-full min-h-screen border-red-500 border-2">
-                <form className={"card card-bordered mx-auto my-auto flex flex-col w-8/12"} method="post" action="/api/lookup" autoComplete="off">
-                    <label className={"label"}>
-                        <span className={"label-text text-lg"}>Client First Name</span>
+                <form className="card card-bordered mx-auto my-auto flex flex-col w-8/12" method="post" action="/api/lookup" autoComplete="off">
+                    <label className="label">
+                        <span className="label-text text-lg">Client First Name</span>
                     </label>
-                    <input type={"text"} name={"firstName"} className={"input input-bordered w-full "}/>
+                    <input type="text" name="firstName" className="input input-bordered w-full "/>
 
-                    <label className={"label"}>
+                    <label className="label">
                         <span className='label-text text-lg'>Client Last Name</span>
                     </label>
-                    <input type={"text"} name={"lastName"} className={"input input-bordered w-full "}/>
+                    <input type={"text"} name={"lastName"} className="input input-bordered w-full "/>
 
                     <label className={"label"}>
-                        <span className={"label-text text-lg"}>Client Birthdate</span>
+                        <span className="label-text text-lg">Client Birthdate</span>
                     </label>
-                    <input type={"date"} name={"birthday"} className={"input input-bordered w-full text-lg"}/>
+                    <input type={"date"} name={"birthday"} className="input input-bordered w-full text-lg"/>
 
-                    <button type={"submit"} className={"btn btn-primary"}>Search</button>
+                    <button type={"submit"} className="btn btn-primary">Search</button>
                 </form>
         </div>
     );
