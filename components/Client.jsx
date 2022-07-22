31d07c7b31884c04dfb80f87a56b3c245a9b974d
Jack Bentsen <jack@bentsen.us>
2022-07-21 18:02:24 -0700

    super basic styling for client cards

--- components/Client.jsx
+++ components/Client.jsx
@@ -1,8 +1,11 @@
 export default function Client({ client }) {
     return (
-        <div className="card bg-base-100">
+        <div className="card bg-base-200 max-w-md p-3 m-3">
             <div className="card-body">
-                <h1 className="card-title">{client.firstName} {client.lastName}</h1>
+                <h1 className="card-title mx-auto text-2xl">{client?.firstName} {client?.lastName}</h1>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-accent btn-sm">Check Out</button>
+                </div>
             </div>
         </div>
     ) 
