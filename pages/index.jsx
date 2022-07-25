49e9f0358d86dc095c976c6337e2e2844e5eb311
Jack Bentsen <jack@bentsen.us>
2022-07-25 15:54:56 -0700

    move form data to new client form ? maybe?

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,7 @@ import { userService } from '../services/user.service';
 import Client from '../components/Client'
 
 import * as Yup from 'yup'
+import Router, { useRouter } from 'next/router';
 
 const clientLookupSchema = Yup.object().shape({
     firstName: Yup.string().required(),
@@ -24,6 +25,7 @@ function parseDateString(value, originalValue) {
 }
 
 export default function Home() {
+    const router = useRouter()
 
     const { register, handleSubmit } = useForm({
         resolver: yupResolver(clientLookupSchema)
@@ -82,6 +84,20 @@ export default function Home() {
         })
     }
 
+    const newClient = (e) => {
+        let basicInfo = {
+            firstName: document.getElementById("firstName").value,
+            lastName: document.getElementById("lastName").value,
+            middleInitial: document.getElementById("middleInitial").value,
+            dateOfBirth: document.getElementById("dateOfBirth").value
+        }
+        console.log(basicInfo)
+        localStorage.setItem('partialClient', JSON.stringify(basicInfo))
+        
+        router.push('/newclient')
+        
+    }
+
     useEffect(() => {
         const subscription = userService.user.subscribe(x => setUsers(x));
         return () => subscription.unsubscribe()
@@ -98,33 +114,34 @@ export default function Home() {
                             {/* First Name */}
                             <div className='p-2 w-60 flex flex-col'>
                             <label className="label label-text">First name</label>
-                            <input type="text" name="firstName" {...register('firstName')} onClick={() => setTouched({...touched, firstName: true})} className="input input-bordered min-w-sm p-2" />
+                            <input id="firstName" type="text" name="firstName" {...register('firstName')} onClick={() => setTouched({...touched, firstName: true})} className="input input-bordered min-w-sm p-2" />
                             {submitErrors.firstName && touched.firstName ? <label className='label-text-alt badge badge-error m-1'>required</label> : null}
                             </div>
 
                             {/* Last Name */}
                             <div className="p-2 w-60 flex flex-col">
                             <label className="label label-text">Last name</label>
-                            <input type="text" name="lastName" {...register('lastName')} onClick={() => setTouched({...touched, lastName: true})} className="input input-bordered min-w-sm p-2" />
+                            <input id="lastName" type="text" name="lastName" {...register('lastName')} onClick={() => setTouched({...touched, lastName: true})} className="input input-bordered min-w-sm p-2" />
                             {submitErrors.lastName && touched.lastName ? <label className="label-text-alt badge badge-error m-1">required</label> : null}
                             </div>
 
                             {/* Middle Initial */}
                             <div className="p-2 w-60 flex flex-col">
                             <label className="label label-text">Middle Initial</label>
-                            <input type="text" name="middleInitial" {...register('middleInitial')} onClick={() => setTouched({...touched, middleInitial: true})} className="input input-bordered min-w-sm p-2" />
+                            <input id="middleInitial" type="text" name="middleInitial" {...register('middleInitial')} onClick={() => setTouched({...touched, middleInitial: true})} className="input input-bordered min-w-sm p-2" />
                             {submitErrors.middleInitial && touched.middleInitial ? <label className='label-text-alt badge badge-error'>Middle Initial must only be one letter</label> : null}
                             </div>
 
                             {/* Date of Birth */}
                             <div className="p-2 w-64 flex flex-col">
                                 <label className="label label-text">Date of Birth</label>
-                                <input type="date" name="dateOfBirth" {...register('dateOfBirth')} placeholder="date" onClick={() => setTouched({...touched, dateOfBirth: true})} className="input input-bordered min-w-sm p-2"></input>
+                                <input id="dateOfBirth" type="date" name="dateOfBirth" {...register('dateOfBirth')} placeholder="date" onClick={() => setTouched({...touched, dateOfBirth: true})} className="input input-bordered min-w-sm p-2"></input>
                                 <label className={`label-text-alt badge badge-error m-1 ${submitErrors.dateOfBirth && touched.dateOfBirth ? "visible" : "hidden"}`}>required</label>
                             </div>
                         </div>
                         <div className='card-actions justify-end my-0'>
                             <button type="submit" className="btn btn-primary p-2 my-2 mr-1">Search</button>
+                            <button type="button" className="btn btn-primary p-2 my-2 mr-1" onClick={newClient}>New Client</button>
                         </div>
                         <div className='divider my-0'></div>
                     </div>
