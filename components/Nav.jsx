dbcc977db66056ac6b4371864063afb4b4f09f26
Atlas238 <jack@bentsen.us>
2022-07-30 19:21:23 -0700

    added in style things!

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -20,38 +20,23 @@ function Nav() {
     }
 
     // Sets the theme
-    function setThemeCupcake() {
-        let html = document.getElementById('html')
-        html.setAttribute("data-theme", "cupcake")
-    }
     function setThemeDark() {
         let html = document.getElementById('html')
         html.setAttribute("data-theme", "dark")
     }
-    function setThemeRainbow() {
-        let html = document.getElementById('html')
-        html.setAttribute("data-theme", "pastel")
-    }
+    
 
     // Only show Nav if logged in - can be done in any page or component as long as you copy useEffect + user state variable
     if (!user) return null
 
     return (
-        <div className="navbar bg-base-200 shadow-sm fixed z-10">
+        <div className="navbar bg-base-200 shadow-sm fixed z-10 h-28">
             <div className="flex-1">
-                <NavLink className="btn btn-ghost normal-case text-xl" href={"/"}>Home</NavLink>
+                <NavLink href={"/"} className="btn btn-secondary h-24"><span><img src={"./Logo_180x180_White.jpg"} className="w-20" /></span>Home</NavLink>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
                     <li><NavLink href={"/checkedin"} className="btn btn-primary text-primary-content m-1">Checked In Clients</NavLink></li>
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-secondary m-1">Theme</label>
-                        <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a onClick={setThemeCupcake}>Light</a></li>
-                            <li><a onClick={setThemeDark}>Dark</a></li>
-                            <li><a onClick={setThemeRainbow}>Rainbow</a></li>
-                        </ul>
-                    </div>
                     <li><a onClick={logout} className="btn btn-accent text-primary-content m-1">Logout</a></li>
                 </ul>
             </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -111,9 +111,9 @@ export default function Home() {
 
     return (
         <div className="flex flex-col min-w-full min-h-screen overflow-x-hidden">
-            <form onSubmit={handleSubmit(submitForm, handleError)} className="card mt-20 mx-auto">
+            <form onSubmit={handleSubmit(submitForm, handleError)} className="card mt-28 mx-auto">
                     <div className='card-body min-w-full'>
-                        <h1 className='card-title text-primary-content my-0'>Lookup Client</h1>
+                        <h1 className='card-title my-0'>Lookup Client</h1>
                         <div className='divider my-0'></div>
                         <div className="form-control flex-row">
 
