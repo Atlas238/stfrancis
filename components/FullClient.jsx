eba1b1cd2647d548f93d1c5cac2ecb59c58a4e95
J.H <59863142+JoenHo@users.noreply.github.com>
2022-08-07 16:15:06 -0700

    Handling null cases

--- components/FullClient.jsx
+++ components/FullClient.jsx
@@ -33,11 +33,11 @@ export default function FullClient({ client }) {
         return (
             <div key={visit.visitID} className="card bg-base-100 shadow-md m-2">
                 <div className=" card-body">
-                <h3 className="card-title font-bold underline underline-offset-1">Visit Date: {visit.visitDate.split("T")[0]}</h3>
+                <h3 className="card-title font-bold underline underline-offset-1">Visit Date: {visit.visitDate?.split("T")[0]}</h3>
                     <ul className="ml-3">
-                        <li><span className="font-semibold">Last Backpack:</span> {visit.lastBackpack.split("T")[0]}</li>
-                        <li><span className="font-semibold">Last Sleeping Bag:</span> {visit.lastSleepingBag.split("T")[0]}</li>
-                        <li><span className="font-semibold">Requests:</span> {visit.request.split("T")[0]}</li>
+                        <li><span className="font-semibold">Last Backpack:</span> {visit.lastBackpack?.split("T")[0]}</li>
+                        <li><span className="font-semibold">Last Sleeping Bag:</span> {visit.lastSleepingBag?.split("T")[0]}</li>
+                        <li><span className="font-semibold">Requests:</span> {visit.request?.split("T")[0]}</li>
                     </ul>
                 </div>
             </div>
