3548bcb84c5bb2655c1b6e7b82fde9f0aba6f9c3
Jack Bentsen <jack@bentsen.us>
2022-08-11 14:55:47 -0700

    fixed weird logo bug... i hope

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 
 import { NavLink } from './NavLink'
+import Image from "next/image";
 import { userService } from '../services/user.service'
-import themes from "daisyui/src/colors/themes";
+
+import logo from '../public/sfhlogo.png'
 
 export { Nav }
 
@@ -31,14 +33,14 @@ function Nav() {
     if (!user) return null
 
     return (
-        <div className="navbar bg-base-200 shadow-sm h-28">
+        <div className="navbar bg-base-200 shadow-sm h-28 fixed z-50">
             <div className="flex-1">
-                <NavLink href={"/"} className="btn btn-ghost h-24"><img src={"./sfhlogo.png"} className="w-52" /></NavLink>
+                <NavLink href={"/"} className="btn btn-ghost h-24"><Image src={logo} width={200} height={90} priority={true}/></NavLink>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
-                    <li><NavLink href={"/checkedin"} className="btn btn-primary text-xl text-primary-content font-thin hover:bg-bluegray m-2 p-2">Checked In Clients</NavLink></li>
-                    <li><a onClick={logout} className="btn btn-accent text-xl text-primary-content font-thin m-2 p-2">Logout</a></li>
+                    <li><NavLink href={"/checkedin"} className="btn btn-primary text-xl text-primary-content font-thin m-2 mt-10">Checked In Clients</NavLink></li>
+                    <li><a onClick={logout} className="btn btn-accent text-xl text-primary-content font-thin m-2 mt-10">Logout</a></li>
                 </ul>
             </div>
         </div>
