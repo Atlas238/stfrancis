9d3dfeaa51be85f7bd20093bfeea4b630ddded3c
Atlas238 <jack@bentsen.us>
2022-08-25 16:45:52 -0700

    more imports to fix my components

--- components/SearchForm.jsx
+++ components/SearchForm.jsx
@@ -1,7 +1,11 @@
+import { RiSearchLine } from 'react-icons/ri'
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+
 import * as Yup from 'yup'
 import { yupResolver } from "@hookform/resolvers/yup"
+import { useRouter } from "next/router"
 
-import { useForm } from "react-hook-form"
 
 const clientLookupSchema = Yup.object().shape({
     firstName: Yup.string(),
@@ -10,7 +14,8 @@ const clientLookupSchema = Yup.object().shape({
     dateOfBirth: Yup.string().matches(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/, {excludeEmptyString: true}),
 }, ['middleInitial', 'middleInitial'])
 
-export default function SearchForm({ setDbClients, setSubmitted, setLoading }) {
+export default function SearchForm({ setDbClients, setSubmitted, setLoading, setLastClients }) {
+    const router = useRouter()
     // State Variables
     const [submitErrors, setSubmitErrors] = useState({
         firstName: null,
@@ -31,6 +36,7 @@ export default function SearchForm({ setDbClients, setSubmitted, setLoading }) {
     // Handled by YUP, sets any errors for custom display messages
     const submitForm = async (data) => {
         setDbClients(null)
+        setLastClients(null)
         setSubmitted(false)
         console.log("Form Submitted")
         setLoading(true)
@@ -44,7 +50,7 @@ export default function SearchForm({ setDbClients, setSubmitted, setLoading }) {
         // let res = await fetch('/api/clients')
         let clients = await res.json()
         if (clients.length != 0) setDbClients(clients)
-        console.log(clients)
+        localStorage.setItem('lastClients', JSON.stringify(clients))
         setLoading(false)
     }   
 
@@ -77,22 +83,22 @@ export default function SearchForm({ setDbClients, setSubmitted, setLoading }) {
     }
 
     return (
-        <form onSubmit={handleSubmit(submitForm, handleError)} className="card mt-28 mx-auto">
+        <form onSubmit={handleSubmit(submitForm, handleError)} className="card mt-28">
             <div className='card-body min-w-full'>
-                <h1 className='card-title text-sfgold ld my-0'>Lookup Client</h1>
+                <h1 className='card-title font-light text-3xl my-0 select-none'>Lookup Client</h1>
                 <div className='divider my-0'></div>
-                <div className="form-control flex-col">
+                <div className="form-control flex-row  justify-center">
                     <div className="flex flex-row">
                         {/* First Name */}
                         <div className='p-2 w-60 flex flex-col'>
-                            <label className="label label-text text-xl">First name</label>
+                            <label className="label label-text text-2xl">First name</label>
                             <input id="firstName" type="text" name="firstName" {...register('firstName')} onClick={() => setTouched({...touched, firstName: true})} className="input input-bordered min-w-sm  bg-white border-gray-400 p-2" />
                             {submitErrors.firstName && touched.firstName ? <label className='label-text-alt badge badge-error m-1'>required</label> : null}
                         </div>
 
                         {/* Last Name */}
                         <div className="p-2 w-60 flex flex-col">
-                            <label className="label label-text text-xl">Last name</label>
+                            <label className="label label-text text-2xl">Last name</label>
                             <input id="lastName" type="text" name="lastName" {...register('lastName')} onClick={() => setTouched({...touched, lastName: true})} className="input input-bordered min-w-sm bg-white border-gray-400 p-2" />
                             {submitErrors.lastName && touched.lastName ? <label className="label-text-alt badge badge-error m-1">required</label> : null}
                         </div>
@@ -100,15 +106,15 @@ export default function SearchForm({ setDbClients, setSubmitted, setLoading }) {
 
                     {/* Date of Birth */}
                     <div className="p-2 w-64 flex flex-col self-center">
-                        <label className="label label-text text-xl">Date of Birth</label>
+                        <label className="label label-text text-2xl">Date of Birth</label>
                             <input id="dateOfBirth" type="date" name="dateOfBirth" {...register('dateOfBirth')} placeholder="date" onClick={() => setTouched({...touched, dateOfBirth: true})} className="input input-bordered min-w-sm  bg-white border-gray-400 p-2" />
                             <label className={`label-text-alt badge badge-error m-1 ${submitErrors.dateOfBirth && touched.dateOfBirth ? "visible" : "hidden"}`}>required</label>
                     </div>
                 </div>
                 
                 <div className='card-actions justify-end my-0'>
-                    <button type="submit" className="btn btn-primary p-2 my-2 mr-1">Search</button>
-                    <button type="button" className="btn btn-primary p-2 my-2 mr-1" onClick={newClient}>New Client</button>
+                    <button type="submit" className="btn btn-primary p-2 my-2 mr-5 font-thin text-lg transition-all hover:scale-125 hover:transition-all"><RiSearchLine /></button>
+                    <button type="button" className="btn btn-primary p-2 my-2 mr-1 font-thin text-lg transition-all hover:transition-all" onClick={newClient}>New Client</button>
                 </div>
                 <div className='divider my-0'></div>
             </div>
