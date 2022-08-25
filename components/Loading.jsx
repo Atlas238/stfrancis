2f0c8d0973a0e8f487f408090f1822d964aef266
Atlas238 <jack@bentsen.us>
2022-08-25 16:44:59 -0700

    loading spinner is component now accepts tailwind as second prop optional

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,8 +1,8 @@
 import { BallTriangle } from "react-loader-spinner";
 
-export default function Loading({ loading }) {
+export default function Loading({ loading, options }) {
     return (
-        <div className={`${loading ? 'visible' : 'hidden'} mx-auto`}>
+        <div className={`${loading ? 'visible' : 'hidden'} mx-auto ${options}`}>
             <BallTriangle width={100} height={100} color="#000000" />
         </div>
     )
