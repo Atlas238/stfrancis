684c099a7efb4df8938e8e84c68ab6e748f04783
Hong Yang <hongyue98@hotmail.com>
2022-08-29 02:01:14 -0700

    Testing login with image

--- pages/login.jsx
+++ pages/login.jsx
@@ -20,17 +20,23 @@ function Login() {
     }, [router]);
 
     return (
-        <div className="hero min-h-screen bg-neutral">
+        <div className="hero min-h-screen">
             <img src={"./sfhlogo.png"} className="image-full w-96 fixed top-0 left-0 select-none" />
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-secondary text-center lg:text-left">
-                    <h1 className=" text-8xl font-bold select-none">Login to Continue</h1>
-                    <p className="py-6 text-3xl text-center select-none">Enter your issued username and password to sign into the main portal</p>
+            <div className="bg-cover">  
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img 
+                        src= {"./sfhpries.jpg"} 
+                        className="absolute right-50 top-0 object-fill z-0"
+                        />
+                    <div className="text-secondary text-center lg:text-left z-50">
+                        <h1 className=" text-8xl text-white font-bold select-none">Login to Continue</h1>
+                        <p className="py-6 text-3xl text-center text-white select-none">Enter your issued username and password to sign into the main portal</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-md mx-5 shadow-2xl bg-accent z-50">
+                        <LoginForm /> 
+                    </div>
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-md mx-5 shadow-2xl bg-accent">
-                    <LoginForm /> 
-                </div>
-            </div>
+            </div>   
         </div>
     );
 }
