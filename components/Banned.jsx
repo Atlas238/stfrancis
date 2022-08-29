35d3866f883339b77ab3211030fe86c74840648b
Jack Bentsen <jack@bentsen.us>
2022-08-29 15:28:32 -0700

    for formatting

--- components/Banned.jsx
+++ components/Banned.jsx
@@ -1,5 +1,5 @@
-export default function Banned() {
+export default function Banned({ hidden }) {
     return (
-        <h1 className="font-bold text-center text-lg bg-red-900 text-primary rounded-md px-4">BANNED</h1>
+        <h1 className={`${hidden ? 'opacity-0' : 'visible'} font-bold text-center text-lg bg-red-900 text-primary rounded-md px-4`}>BANNED</h1>
     )
 }
