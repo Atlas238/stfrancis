b693018a138ffdf844a6066d07194a5ec114cb3c
J.H <59863142+JoenHo@users.noreply.github.com>
2022-08-29 02:16:31 -0700

    Change numFamily to numKids to match with backend property name

--- pages/updateclient/[id].jsx
+++ pages/updateclient/[id].jsx
@@ -45,7 +45,7 @@ export default function updateclient({ data }) {
         setUpdateClient(updateClient) //save in submit function so we can CALL submitForm in second button, but use data from state in other function (ie go to checkin)
 
         // Update client request to DB
-        let response = await fetch(`https://stfrancisone.herokuapp.com/home/updateClientByID?clientID=${id}&firstName=${updateClient.firstName}&lastName=${updateClient.lastName}&middleInitial=${updateClient.middleInitial}&birthdate=${updateClient.dateOfBirth.toISOString().split('T')[0]}&gender=${updateClient.gender}&race=${updateClient.race}&zipcode=${updateClient.postalCode}&banned=${updateClient.banned}&numFamily=${updateClient.familySize}`)
+        let response = await fetch(`https://stfrancisone.herokuapp.com/home/updateClientByID?clientID=${id}&firstName=${updateClient.firstName}&lastName=${updateClient.lastName}&middleInitial=${updateClient.middleInitial}&birthdate=${updateClient.dateOfBirth.toISOString().split('T')[0]}&gender=${updateClient.gender}&race=${updateClient.race}&zipcode=${updateClient.postalCode}&banned=${updateClient.banned}&numKids=${updateClient.familySize}`)
         // if successful
         if(response.ok && response.status===200){
             alert("Successfully Saved")
