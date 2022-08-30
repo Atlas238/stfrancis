eff06eb3bc9fad6085fc66ba23c448baf592d193
Jack Bentsen <jack@bentsen.us>
2022-08-30 10:57:55 -0700

    clear lastclients when someone comes to settings fixes a lot of settings related issues

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -1,6 +1,10 @@
 import SettingForm from "components/SettingForm";
+import { useEffect } from "react";
 
 export default function settings() {
+    useEffect(()=> {
+        localStorage.removeItem('lastClients')
+    }, [localStorage]) 
     return (
         <div>
             <h1 className="pt-40 text-4xl text-primary-content ml-10 select-none">Settings</h1>
