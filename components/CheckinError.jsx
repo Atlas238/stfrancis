eeb2cd41ff3d3e5c7e32836f15ca778723a29456
Jack Bentsen <jack@bentsen.us>
2022-08-30 10:52:59 -0700

    Error Message if client is already checked in

---
+++ components/CheckinError.jsx
@@ -0,0 +1,8 @@
+export default function CheckinError() {
+    return (
+        <div className="pt-32">
+            <h2 className="text-center text-2xl font-semibold">Whoops!</h2>
+            <p className="text-center text-lg p-4">Looks like this person has already been checked in. Go to the Checked In page and look for them there!</p>
+        </div>
+    )
+}
